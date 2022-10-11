6a219c9f6d9cb3676eb21a187c8d337a333805bf
Adi Nicolae <nicolae.adrian.m@gmail.com>
2022-10-11 03:05:31 +0300

    'Email' to 'TempEmail'!  - Changed headers to Pascal Case.  - Changed 'None' comparator to 'is' instead of '=='.  - Converted for loop into list comprehension, where possible.  - Formatted class through 'black'.  - Immediately returned in-line variables, where possible.  - Ordered imported libraries.  - Renamed class from 'Email' to 'TempEmail'.  - Used the common base class for all non-exit exceptions (Exception).

--- temp_mail.py
+++ temp_mail.py
@@ -5,38 +5,51 @@ import string
 
 
 class TempEmail:
-    def __init__(self, proxy: str= None, timeout: int=15) -> None:
-        self.session = Client(headers={'content-type': 'application/json'}, timeout=timeout, proxies=proxy)
-        self.base_url = 'https://api.mail.tm'
-    
+    def __init__(self, proxy: str = None, timeout: int = 15) -> None:
+        self.session = Client(
+            headers={"Content-Type": "application/json"}, timeout=timeout, proxies=proxy
+        )
+        self.base_url = "https://api.mail.tm"
+
     def get_domains(self) -> list:
-        domains: list = []
-        
-        for item in self.session.get(f'{self.base_url}/domains').json()['hydra:member']:
-            domains.append(item['domain'])
+        return [
+            item["domain"]
+            for item in self.session.get(f"{self.base_url}/domains").json()[
+                "hydra:member"
+            ]
+        ]
 
-        return domains
+    def get_mail(
+        self,
+        name: str = "".join(choice(string.ascii_lowercase) for _ in range(15)),
+        password: str = None,
+        domain: str = None,
+    ) -> str:
+        mail: str = f"{name}@{domain if domain != None else self.get_domains()[0]}"
+        response: int = self.session.post(
+            f"{self.base_url}/accounts", json={"address": mail, "password": mail}
+        ).status_code
 
-    def get_mail(self, name: str = ''.join(choice(string.ascii_lowercase) for _ in range(15)), password: str= None, domain: str = None) -> str:
-        mail: str =  f'{name}@{domain if domain != None else self.get_domains()[0]}'
-        response: int = self.session.post(f'{self.base_url}/accounts', json={'address': mail, 'password': mail}).status_code
-        
         try:
             if response == 201:
-                token = self.session.post(f'{self.base_url}/token', json={'address': mail, 'password': mail if password == None else password}).json()['token']
-                self.session.headers['authorization'] = f'Bearer {token}'
+                token = self.session.post(
+                    f"{self.base_url}/token",
+                    json={
+                        "address": mail,
+                        "password": mail if password is None else password,
+                    },
+                ).json()["token"]
+
+                self.session.headers["Authorization"] = f"Bearer {token}"
                 return mail
-        except:
-            return 'Email creation error.'
-    
+        except Exception:
+            return "Email creation error."
+
     def fetch_inbox(self):
-        response = self.session.get(f'{self.base_url}/messages').json()['hydra:member']
-        return response
-    
+        return self.session.get(f"{self.base_url}/messages").json()["hydra:member"]
+
     def get_message(self, message_id: str):
-        response = self.session.get(f'{self.base_url}/messages/{message_id}').json()
-        return response
-    
+        return self.session.get(f"{self.base_url}/messages/{message_id}").json()
+
     def get_message_content(self, message_id: str):
-        response = self.get_message(message_id)['text']
-        return response
+        return self.get_message(message_id)["text"]
